522f5c5ae70a68a4b3d524482ef7e00e235ed50e
Drew Walsworth <walsworth.drew96@gmail.com>
2020-10-11 09:52:23 -0500

    update

--- shelve_test.py
+++ shelve_test.py
@@ -1,10 +1,9 @@
 from person import Person, Manager
+import shelve
 
 bob = Person('Bob Smith')
 sue = Person('Sue Jones')
 tom = Manager('Tom Jones', 50000)
-
-import shelve
 
 db = shelve.open('persondb')
 for obj in (bob, sue, tom):
