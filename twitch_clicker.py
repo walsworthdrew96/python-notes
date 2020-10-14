d931d1adce7ba620a3de846abb15824d7e5137fb
Drew Walsworth <walsworth.drew96@gmail.com>
2020-10-14 09:06:33 -0500

    update

--- twitch_clicker.py
+++ twitch_clicker.py
@@ -2,17 +2,27 @@ import pyautogui as pya
 import time
 
 pya.FAILSAFE = False
-
 p = (1667, 1011)
 u = (1789, 1011)
+
+rightMonitor = None
+while rightMonitor != True and rightMonitor != False:
+    rightMonitor = bool(input('Enter "True" or "False":'))
+    if rightMonitor != True and rightMonitor != False:
+        print('Monitor toggle not set, try again.')
 
 # while True:
 #     print(pya.position())
 #     time.sleep(1)
 
 time.sleep(5)
-
-while True:
-    pya.click(p[0], p[1])
-    pya.click(u[0], u[1])
-    time.sleep(30)
+if rightMonitor:
+    while True:
+        pya.click(p[0], p[1])
+        pya.click(u[0], u[1])
+        time.sleep(30)
+else:
+    while True:
+        pya.click(p[0] - 1920, p[1])
+        pya.click(u[0] - 1920, u[1])
+        time.sleep(30)
